81b463610b24b1614c69ed5576e51e7141a55a50
Shua Miller <thejoshmill@gmail.com>
2020-12-08 09:17:59 -0700

    Add variables and functions for gallery.

---
+++ script.js
@@ -0,0 +1,30 @@
+/* let graphicsGallery = document.getElementById('gallery-image');
+let body = document.getElementsByName('body');
+
+function openGallery() {
+    let overlay = document.createElement('div');
+    overlay.setAttribute('id', 'overlay');
+}
+
+graphicsGallery.addEventListener('click', ) */
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusSlides(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    const slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let i = 0; i < x.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
